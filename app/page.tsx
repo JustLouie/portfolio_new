fe7ea9f78a2f9de48965a613a62d30a73c0cdc23
George <giorgi.shalamberidze2@gmail.com>
2026-01-16 12:26:30 +0400

    Started creating Button component for welcome section on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,22 @@
+import Container from "@/components/Container";
+
 
 export default function Home() {
   return (
     <div className="home-page ">
-     
+      <section className="welcome-section">
+        <Container>
+          <div>
+            <div>
+              <h1>George is a <span>Software Engineer</span></h1>
+              <p>
+                He crafts responsive websites where technologies meet creativity
+              </p>
+
+            </div>
+          </div>
+        </Container>
+      </section>
     </div>
   );
 }
